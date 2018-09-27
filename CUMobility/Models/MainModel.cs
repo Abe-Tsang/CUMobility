5fabaca3092abb36bf0fc59ee6fd7a1c2d76338d
Abe-Tsang <chts3450@colorado.edu>
2018-09-27 07:45:41 -0600

    Added problem reporting

---
+++ CUMobility/Models/MainModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CUMobility.Models
+{
+    public class MainModel
+    {
+        public String ProblemLocation { get; set; }
+        public String ProblemKind { get; set; }
+    }
+}
